ba98a5e1f2ad0e140827a8b430d94ab3504cab49
Andreas Åkerlöf <a.akerlof@gmail.com>
2020-11-04 14:07:27 +0100

    feat(api): base api and getWeatherData added

---
+++ src/api/client.js
@@ -0,0 +1,7 @@
+const API_URL = process.env.REACT_APP_API_URL;
+
+export default async function client(endpoint) {
+  const res = await fetch(`${API_URL}/${endpoint}`);
+  const data = await res.json();
+  return res.ok ? data : Promise.reject(data);
+}
